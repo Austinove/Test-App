ddfa5fb317e179defd4c7b004fc0e3f810424a2d
Bryan Austin <bryanovicaustenove@gmail.com>
2022-09-05 05:51:35 +0300

    password simple validation

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -2,20 +2,24 @@ import React, {useState} from 'react';
 import {View, Text, StyleSheet, TextInput} from 'react-native';
 
 const TextScreen = () => {
-  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <View>
-      <Text>Enter Name: </Text>
+      <Text>Enter Password: </Text>
       <TextInput
         style={styles.input}
         autoCapitalize="none"
         autoCorrect={false}
-        value={name}
+        value={password}
         onChangeText={newValue => {
-          setName(newValue);
+          setPassword(newValue);
         }}
       />
-      <Text>My Name is: {name}</Text>
+      {password.length < 5 ? (
+        <Text style={styles.password}>
+          Password Needs atlease Five (5) characters
+        </Text>
+      ) : null}
     </View>
   );
 };
@@ -25,6 +29,10 @@ const styles = StyleSheet.create({
     margin: 15,
     borderColor: 'black',
     borderWidth: 1,
+  },
+  password: {
+    fontSize: 12,
+    color: 'red',
   },
 });
 
